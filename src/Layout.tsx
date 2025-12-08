4edf3ff3ebfce9b366ad6f3466948a50bc6ed102
apan <23662018@kthcm.edu.vn>
2025-12-08 15:42:26 +0000

    aaaaaaaa

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -296,7 +296,7 @@ const Layout = () => {
 
           <div style={{ flex: "1 1 500px" }}>
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6591.5472..."
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31355.82558029258!2d106.65002909379822!3d10.77463956992026!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f4b3330bcc7%3A0x4db964d76bf6e18e!2sSaigon%20Zoo%20%26%20Botanical%20Gardens!5e0!3m2!1sen!2s!4v1765206702396!5m2!1sen!2s"
               width="100%"
               height="320"
               style={{
